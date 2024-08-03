191ed7ec682346ded4a7039e28dd8759c40ef10e
marijadjordjevic724 <94449560+marijadjordjevic724@users.noreply.github.com>
2024-08-03 12:34:04 +0200

    Solution 25
    Finished

--- Code/solution25.py
+++ Code/solution25.py
@@ -20,8 +20,8 @@ class Solution25(Scene):
         question = VGroup(
             MathTex(r"\text{If one says that the matrix A has a trivial kernel,}", color = first_color),
             MathTex(r"\text{what does this mean?}",color=first_color),
-            MathTex(r"\text{a) Ker(A)=0}", color=first_color),
-            MathTex(r"\text{b) Ker(A)=\{0\}}", color=first_color),
+            MathTex(r"\text{a) Ker(A)= 0}", color=first_color),
+            MathTex(r"\text{b) Ker(A)= \{0\}}", color=first_color),
             MathTex(r"\text{c) it depends}", color=first_color)
         ).arrange(DOWN, center=False, aligned_edge=LEFT).shift(1.5*UP)
         VGroup(question[2:]).shift(RIGHT)
@@ -41,26 +41,33 @@ class Solution25(Scene):
         self.play(Write(question[4]))
         self.wait(2)
         self.play(question[3].animate.set_color(second_color))
-        self.wait(1)
-        question[3].set_color(second_color)
+        self.wait(2)
         self.play(Transform(VGroup(question,box), question_scaled))
         self.wait(1)
 
         # Showing what Kernel is
+        kernel_definition = VGroup(
+            MathTex(r"\text{Kernel is a subspace of the domain,}", color = first_color),
+            MathTex(r"\Rightarrow \text{ it is a set of elements.}",color=first_color),
+            MathTex(r"\text{ Ker(A) = \{...\}}",color=second_color)
+        ).arrange(DOWN, center=False, aligned_edge=LEFT).scale(0.8).next_to(question_scaled, RIGHT)
+        kernel_definition[2].shift(RIGHT)
+        kernel_definition.shift(0.2*UP)
+
         ellipse_1 = VGroup(
             Ellipse(width=2.0, height=4.0, color=first_color),
-            MathTex(r"x", color = first_color)
-            ).arrange(UP).shift(RIGHT+1.5*DOWN)
+            MathTex(r"\text{Domain}", color = first_color,font_size=DEFAULT_FONT_SIZE*0.8)
+            ).arrange(UP).shift(1.5*RIGHT+1.5*DOWN)
         
         ellipse_2 = VGroup(
             Ellipse(width=2.0, height=4.0, color=first_color),
-            MathTex(r"Ax", color = first_color)
-            ).arrange(UP).shift(5*RIGHT+1.5*DOWN)
+            MathTex(r"\text{Codomain}", color = first_color,font_size=DEFAULT_FONT_SIZE*0.8)
+            ).arrange(UP).shift(5.5*RIGHT+1.5*DOWN)
         
         arrow = VGroup(
             Arrow(color = first_color),
             MathTex(r"A", color = first_color)
-            ).arrange(UP).next_to(ellipse_1, RIGHT+0.2*UP)
+            ).arrange(UP).scale(0.8).next_to(ellipse_1, RIGHT+0.1*UP)
         
         ellipse_3 = VGroup(
             Ellipse(width=1.0, height=2.0, color=second_color),
@@ -71,10 +78,12 @@ class Solution25(Scene):
             Dot(color = second_color),
             MathTex(r"0", color = second_color,font_size=DEFAULT_FONT_SIZE*0.7)
             ).arrange(UP).move_to(ellipse_2)
+        
         lines = VGroup(
             Line(start=ellipse_3[0].get_top(), end=dot[0].get_center(), color = second_color),
             Line(start=ellipse_3[0].get_bottom(), end=dot[0].get_center(), color = second_color))
-        
+        lines2 = lines.copy()
+
         self.play(
             Create(ellipse_1),
             Create(ellipse_2))
@@ -85,14 +94,48 @@ class Solution25(Scene):
         self.wait(0.5)
         self.play(Create(lines), Create(dot))
         self.wait(2)
+        self.play(Create(kernel_definition[0]))
+        self.wait(0.5)
+        self.play(Create(kernel_definition[1]))
+        self.wait(0.5)
+        self.play(Create(kernel_definition[2]))
+        self.wait(2)
 
         trivial_definition = VGroup(
-            MathTex(r"\text{Trivial kernel means that Ker(A)}", color = first_color),
-            MathTex(r"\text{contains only one element.}",color=first_color)
-        ).arrange(DOWN, center=False, aligned_edge=LEFT).move_to(question_scaled, DOWN)
-
-        ellipse__small = VGroup(
+            MathTex(r"\text{Trivial kernel means that Ker(A) }", color = first_color),
+            MathTex(r"\text{contains only one element, which is 0.}",color=first_color),
+            MathTex(r"\Rightarrow\text{ Ker(A) = \{0\}}",color=second_color),
+            MathTex(r"\text{Writing Ker(A)= 0 indicates that }", color = first_color),
+            MathTex(r"\text{kernel is an element of the domain,}",color=first_color),
+            MathTex(r"\text{which is improper, because }",color=first_color),
+            MathTex(r"\text{it is a subset, not an element.",color=first_color),
+        ).arrange(DOWN, center=False, aligned_edge=LEFT).scale(0.8).next_to(question_scaled, DOWN)
+        trivial_definition.shift(0.5*RIGHT)
+        trivial_definition[2].shift(RIGHT)
+        trivial_definition[3:].shift(DOWN)
+        dot2 = VGroup(
             Dot(color = second_color),
             MathTex(r"0", color = second_color,font_size=DEFAULT_FONT_SIZE*0.7)
-        ).arrange(UP).move_to(ellipse_1)
+            ).arrange(UP).move_to(ellipse_3)
+        line2 = Line(start=dot2[0].get_center(), end=dot[0].get_center(), color = second_color)
+        
+        self.play(Create(trivial_definition[0]))
+        self.wait(0.8)
+        self.play(Create(trivial_definition[1]), Create(dot2))
+        self.wait(1)
+        self.play(Create(trivial_definition[2]))
+        self.wait(2)
+        self.play(Create(trivial_definition[3]))
+        self.wait(0.3)
+        self.play(Create(trivial_definition[4]), ReplacementTransform(lines, line2), FadeOut(ellipse_3))
+        self.wait(1)
+        self.play(Create(trivial_definition[5]))
+        self.wait(0.3)
+        self.play(Create(trivial_definition[6]), FadeIn(ellipse_3),ReplacementTransform(line2, lines2) )
+        self.wait(2)
+
+        self.play(question[3].animate.set_color(second_color))
+        self.wait(2)
+        self.play(*[FadeOut(mob)for mob in self.mobjects])
+
 
